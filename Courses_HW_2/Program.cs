b03098d428d434ef12a7f5e815cb2298b7798553
Lestrn <ilestrn@gmail.com>
2023-04-22 17:25:04 +0300

    -1 instaed of 0 in Index of max number method

--- Courses_HW_2/Program.cs
+++ Courses_HW_2/Program.cs
@@ -4,8 +4,7 @@
     {
         private static void Main(string[] args)
         {
-            StringHelper stringHelper = new StringHelper("     ,543Hel50000,2lo! Today i would like to add 5 and 10 and 5,5 and 5.5 lol why not xD also i want to type some  comas" +
-                "to break ur program ,,,,,,,, and points cuz why not.... and some stupid50000,2 numbers 11,,,,,12 13..14 15gg ,52"); // Was trying to find every possible way to get an Exception
+            StringHelper stringHelper = new StringHelper(""); // Was trying to find every possible way to get an Exception
             List<double> foundNumbers = stringHelper.GetNumbersFromText();
             Console.WriteLine("Found Numbers:");
             foreach (var number in foundNumbers)
